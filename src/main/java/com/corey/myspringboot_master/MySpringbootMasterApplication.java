588e37ca4235efc717b0d4643eacc8cbecf5b56a
Corey Adams <coreyadams96@outlook.com>
2022-05-31 19:42:50 -0400

    Initial commit

---
+++ src/main/java/com/corey/myspringboot_master/MySpringbootMasterApplication.java
@@ -0,0 +1,13 @@
+package com.corey.myspringboot_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corey/myspringboot_master/MySpringbootMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corey.myspringboot_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
